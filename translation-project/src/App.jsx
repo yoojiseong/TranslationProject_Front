10d0e995a5b88b5c23a24a7d81a47bde2b1b5b15
yoojiseong <yujiseong588@gmail.com>
2025-08-27 21:35:22 +0900

    화면 수정중

--- translation-project/src/App.jsx
+++ translation-project/src/App.jsx
@@ -1,37 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {Outlet, Route, Routes} from 'react-router-dom';
-import Register from "./components/Register.jsx";
-import Login from "./components/Login.jsx";
-import Home from "./components/Home.jsx";
-import HomePageTest from "./components/HomepageTest.jsx";
-import ToolsPage from "./components/ToolsPage.jsx";
-import UserNav from "./components/UserNav.jsx";
-import MyPage from "./components/MyPage.jsx";
-import Footer from "./components/Footer.jsx";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import HomePageTest from "./components/HomepageTest.jsx";
+import Login from "./components/Login.jsx";
+import Register from "./components/Register.jsx";
+import ToolsPage from "./components/ToolsPage.jsx";
+import MyPage from "./components/MyPage.jsx";
+
+// ✨ App.css는 App.jsx에서 직접 import하여 레이아웃을 제어합니다.
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-      <div className="app-container">
-          <Header />
-          <main className="main-content">
-              <Routes>
-                  <Route path="/home" element={<Home />} />
-                  <Route path="/register" element={<Register />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route index element={<HomePageTest />} />
-                  <Route path="/toolspage" element={<ToolsPage />} />
-                  <Route path="/mypage" element={<MyPage />} />
-              </Routes>
-          </main>
-          <Footer />
-      </div>
-  )
+    return (
+        <div className="app-container">
+            <Header />
+            <main className="main-content">
+                <Routes>
+                    <Route index element={<HomePageTest />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/toolspage" element={<ToolsPage />} />
+                    <Route path="/mypage" element={<MyPage />} />
+                </Routes>
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
-export default App
+export default App;
